fe1a46877a2096c7a6647db7b8f52e852b85b856
HyeonWooJo <HyeonWooJo@github.com>
2022-08-15 21:34:11 +0900

    Fix: Modeling

--- companies/models.py
+++ companies/models.py
@@ -6,7 +6,7 @@ class Company(models.Model):
     name     = models.CharField(null=True, max_length=100)
     country  = models.CharField(null=True, max_length=100)
     location = models.CharField(null=True, max_length=100)
-    posting  = models.ForeignKey(Posting, on_delete=models.CASCADE)
+    posting  = models.ForeignKey(Posting, null=True, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'companies'
