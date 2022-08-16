1d579f79a88921137e002aacd7523060cbc02068
HyeonWooJo <HyeonWooJo@github.com>
2022-08-16 09:37:54 +0900

    Fix: modeling

--- postings/models.py
+++ postings/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+from companies.models import Company
+
 class Posting(models.Model):
     position = models.CharField(null=True, max_length=100)
     reward   = models.IntegerField(default=0)
     content  = models.CharField(null=True, max_length=300)
     stack    = models.CharField(null=True, max_length=100)
+    company  = models.ForeignKey(Company, null=True, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'postings'
